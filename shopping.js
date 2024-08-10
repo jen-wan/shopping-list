282c9276fe5a74ec614b006887b88c7de53b25f3
jennyjwan2@gmail.com <jennyjwan2@gmail.com>
2024-08-10 11:41:39 -0700

    save my project

--- shopping.js
+++ shopping.js
@@ -5,13 +5,16 @@ const app = express(); // Create the Express application object `app`.
 const host = "localhost"; // define host to which app listens for HTTP connections.
 const port = 3002; // define the port to which the app listens for HTTP connctions.
 
+// Static data for initial testing
+let shoppingLists = require("./lib/seed-data");
+
 app.set("views", "./views"); // tell Express where to find view templates
 app.set("view engine", "pug"); // Tell express to use Pug as the view engine.
 
 app.use(morgan("common")); // Enable logging with Morgan.
 
 app.get("/", (req, res) => { // primary route for this application
-  res.render("lists"); // renders the lists.pug view
+  res.render("lists", { shoppingLists }); // renders the lists.pug view
 });
 
 // Listener
