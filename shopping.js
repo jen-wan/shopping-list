e86b252a365f44a969efffa3deee2efac26ada47
jennyjwan2@gmail.com <jennyjwan2@gmail.com>
2024-08-09 20:34:47 -0700

    save changes

---
+++ shopping.js
@@ -0,0 +1,20 @@
+const express = require("express"); // Load the `express` module.
+const morgan = require("morgan"); // Load the `morgan` module.
+
+const app = express(); // Create the Express application object `app`. 
+const host = "localhost"; // define host to which app listens for HTTP connections.
+const port = 3002; // define the port to which the app listens for HTTP connctions.
+
+app.set("views", "./views"); // tell Express where to find view templates
+app.set("view engine", "pug"); // Tell express to use Pug as the view engine.
+
+app.use(morgan("common")); // Enable logging with Morgan.
+
+app.get("/", (req, res) => { // primary route for this application
+  res.render("lists"); // renders the lists.pug view
+});
+
+// Listener
+app.listen(port, host, () => {
+  console.log(`Shopping is listening on port ${port} of ${host}!`);
+});
